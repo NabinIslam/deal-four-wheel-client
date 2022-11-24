af7aa06b96cbb30175d285eb261b19b2ed13e3c3
Nabin Islam <sinabin62246@gmail.com>
2022-11-24 11:46:04 +0600

    404 page done

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Button } from 'flowbite-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="h-[90vh] flex flex-col justify-center items-center">
+      <h1 className="font-extrabold text-[200px] m-0 p-0">404</h1>
+      <p className="font-semibold text-2xl">Opps! This page not found</p>
+      <Link to={'/'}>
+        <Button className="my-8">Go back to Homepage</Button>
+      </Link>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
