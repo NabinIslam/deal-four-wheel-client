93a61bf02f6f0bb2989beba4d62c74c4099fcf1a
Nabin Islam <sinabin62246@gmail.com>
2022-11-24 12:15:29 +0600

    home page banner done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import car1 from '../assets/home page slider images/car 1.jpg';
+import car2 from '../assets/home page slider images/car 2.jpg';
+import car3 from '../assets/home page slider images/car 3.jpg';
+import { Carousel } from 'flowbite-react';
+
+const Banner = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold my-8">
+        AFFORDABLE AND LIKE NEW CARS
+      </h1>
+      <div className="max-w-3xl mx-auto h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slideInterval={1500}>
+          <img src={car1} alt="..." />
+          <img src={car2} alt="..." />
+          <img src={car3} alt="..." />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
