8eea98877774f1bff7b714272c0ed4608a2aff91
Nabin Islam <sinabin62246@gmail.com>
2022-11-24 11:57:59 +0600

    navbar setup done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
+import { Navbar } from 'flowbite-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div>this is header</div>;
+  return (
+    <div className="shadow">
+      <div className="container mx-auto">
+        <Navbar fluid={true} rounded={true}>
+          <Link to="/">
+            <h1 className="font-extrabold text-xl">DealFourWheel</h1>
+          </Link>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Link to="/" active={true}>
+              Home
+            </Link>
+            <Link to="/">About</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Pricing</Link>
+            <Link to="/">Contact</Link>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
